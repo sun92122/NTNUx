c52580b7a86514f32a4d62e903e4c83ee1c96865
sun92122 <sun920122@gmail.com>
2024-07-05 17:29:13 +0800

    ✨ Nuxt 網頁初始化

---
+++ ntnu_course/pages/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <AgGridVue
+    :rowData="rowData"
+    :columnDefs="colDefs"
+    style="height: 500px"
+    class="ag-theme-quartz"
+  >
+  </AgGridVue>
+</template>
+
+<script>
+import { ref } from "vue";
+import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the Data Grid
+import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the Data Grid
+import { AgGridVue } from "ag-grid-vue3"; // Vue Data Grid Component
+
+export default {
+  name: "App",
+  components: {
+    AgGridVue, // Add Vue Data Grid component
+  },
+  setup() {
+    // Row Data: The data to be displayed.
+    const rowData = ref([]);
+
+    // Column Definitions: Defines the columns to be displayed.
+    const colDefs = ref([
+      { field: "dept_chiabbr" },
+      { field: "chn_name" },
+      { field: "time_inf" },
+      { field: "credit" },
+      { field: "serial_no" },
+    ]);
+
+    onMounted(async () => {
+      rowData.value = [];
+      // 0-45
+      for (let i = 0; i < 46; i++) {
+        const data = await fetchData(i);
+        rowData.value = rowData.value.concat(data);
+      }
+    });
+
+    const fetchData = async (i) => {
+      // Fetch data from the server
+      const response = await fetch("response_content_" + i + ".min.json");
+      // const response = await fetch("https://www.ag-grid.com/example-assets/space-mission-data.json");
+      return response.json();
+    };
+
+    return {
+      rowData,
+      colDefs,
+    };
+  },
+};
+</script>
