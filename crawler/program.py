3e614e5bedc4f63aba3b0ec7d75f324c67f59bd0
magical <sun920122@gmail.com>
2026-03-10 05:29:25 +0800

    🚧 調整學分學程資料解碼格式

--- crawler/program.py
+++ crawler/program.py
@@ -1,45 +1,18 @@
+from tool.analysis import get_program_dir
+from tool.conversion_program import get_program_conversion
 import argparse
 import json
 import os
 
 
-def get_program_conversion(program_codes: str, program_dir: dict[str, str]) -> str:
-    """將學程代碼轉換為學程名稱
-
-    Args:
-        program_codes (str): 學程代碼，可能包含多個代碼，以 "/" 分隔
-        program_dir (dict[str, str]): 學程代碼與學程名稱的對照字典
-
-    Returns:
-        str: 轉換後的學程名稱，若無法轉換則返回原始代碼
-    """
-    if program_codes is None or program_codes == "":
-        return ""
-    for program_code in program_codes.split("/"):
-        if program_code in program_dir:
-            return program_dir[program_code]
-        elif (program_code + "學程") in program_dir:
-            return program_dir[program_code + "學程"]
-        elif (program_code.replace("教育學程", "學程")) in program_dir:
-            return program_dir[program_code.replace("教育學程", "學程")]
-        elif program_code.startswith("Z") and len(program_code) == 4:
-            # already converted, skip
-            return program_code
-    # print(
-    #     f"找不到對應的學程名稱，請檢查 wrangler.toml 配置，原始代碼: {program_codes}")
-    raise ValueError(
-        f"找不到對應的學程名稱，請檢查 wrangler.toml 配置，原始代碼: {program_codes}")
-
-
 def main(yt: str):
-    from tool.analysis import get_program_dir
     config_path = os.path.join(os.path.dirname(
-        __file__), "..", "..", "wrangler.toml")
+        __file__), "..", "wrangler.toml")
     program_dir = get_program_dir(config_path)
 
     # check if yt is exist in original_data directory
     data_path = os.path.abspath(os.path.join(
-        os.path.dirname(__file__), "..", "original_data", yt))
+        os.path.dirname(__file__), "original_data", yt))
     if not os.path.exists(data_path):
         print(f"找不到資料目錄: {data_path}")
         return
